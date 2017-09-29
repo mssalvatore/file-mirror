e7909d22fd5971602b72d6bdbd0074b34078a959
Mike Salvatore <mike.s.salvatore@gmail.com>
2017-09-28 21:18:36 -0400

    Add RsyncAction
    An RsyncAction takes a preconfigured AFM\Rsync\Rsync object, source
    path, and destination path and performs a sync on call to execute().

---
+++ tests/Actions/RsyncActionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace mssalvatore\FileMirror\Test\Actions;
+
+use \mssalvatore\FileMirror\Actions\RsyncAction;
+use \PHPUnit\Framework\TestCase;
+
+class RsyncActionTest extends TestCase
+{
+    const SOURCE_PATH =  "/root/testSourcePath";
+    const DESTINATION_PATH = "/root/testDestinationPath";
+    protected function setUp()
+    {
+        $this->mockRsyncClient = $this->createMock(\AFM\Rsync\Rsync::class);
+        $this->rsyncAction = new RsyncAction($this->mockRsyncClient, self::SOURCE_PATH, self::DESTINATION_PATH);
+    }
+
+    public function testExecute()
+    {
+        $this->mockRsyncClient
+             ->expects($this->once())
+             ->method("sync")
+             ->with($this->equalTo(self::SOURCE_PATH), $this->equalTo(self::DESTINATION_PATH));
+
+        $this->rsyncAction->execute();
+    }
+}
